3d5bdd9f106d7016ac955f92e0d21ae149a71d25
nperon <nperon@gmail.com>
2019-03-17 01:11:23 +0100

    disk keeps moving as long as mouse button remains pressed

--- src/app/editable-svg/editable-svg.component.ts
+++ src/app/editable-svg/editable-svg.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store'; 
 import { Observable } from 'rxjs/Observable';
 import { moveLeftAction } from './store/editable-svg.reducer';
+import { interval } from 'rxjs';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-editable-svg',
   templateUrl: './editable-svg.component.html',
   styleUrls: ['./editable-svg.component.css']
 })
-export class EditableSvgComponent implements OnInit {
+export class EditableSvgComponent implements OnInit, OnDestroy {
   editableSvgState: Observable<{ xCoordinate: number, yCoordinate: number }>;
   xCoordinate: number;
   yCoordinate: number;
   radius = 50;
+  subscription: Subscription;
 
   constructor(
     private store: Store<{editableSvg: { xCoordinate: number, yCoordinate: number }}>
@@ -22,7 +25,17 @@ export class EditableSvgComponent implements OnInit {
     this.editableSvgState = this.store.select('editableSvg');
   }
 
-  moveLeft() {
-    this.store.dispatch(new moveLeftAction(5));
+  onMouseDown() {
+    const clock = interval(400);
+    this.store.dispatch(new moveLeftAction(4));
+    this.subscription = clock.subscribe((number: Number) => { this.store.dispatch(new moveLeftAction(4)) });
+  }
+
+  onMouseUpOrOut() {
+    if (this.subscription !== undefined) { this.subscription.unsubscribe() };
+  }
+
+  ngOnDestroy() {
+    if (this.subscription !== undefined) { this.subscription.unsubscribe() };
   }
 }
